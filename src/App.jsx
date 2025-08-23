64de617a8c85f454e2f5a7dbdd826ee55682f3b4
WatchOut-RPG <sourav060606@gmail.com>
2025-08-23 23:43:45 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Auth from "./Components/Auth/Auth"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+     <Auth/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Auth/Auth.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginYourAcc from "./LoginPages/LoginYourAcc";
+import CreateAcc from "./LoginPages/CreateAcc";
+
+export default function Auth() {
+  return (
+    <>
+      <CreateAcc/>
+    </>
+  );
+}
